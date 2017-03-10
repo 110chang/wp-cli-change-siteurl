90ae995b42d8e38d9b4a39119993c45a5ea44a95
110chang <u@110chang.com>
2017-03-10 13:32:30 +0900

    Remove specific infomation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir wp && cd wp
 wp core download --locale=ja
 wp core config --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=$DBHOST --dbprefix=$DBPREFIX
-wp core install --url=$WPURL/wp --title=CPU --admin_user=wordpress --admin_password=password --admin_email=u@110chang.com
+wp core install --url=$WPURL/wp --title=YOUR_TITLE --admin_user=wordpress --admin_password=password --admin_email=email@example.com
 wp option update home $WPURL
 cp index.php ../index.php && cd ..
 sed -i "" "s|/wp-blog-header|/wp/wp-blog-header|" ./index.php
