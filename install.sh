bedac9311ea2fc1448ca5b3ae4daf693f7c96bc7
110chang <u@110chang.com>
2017-03-10 13:42:15 +0900

    Fix exposure email

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir wp && cd wp
 wp core download --locale=ja
 wp core config --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=$DBHOST --dbprefix=$DBPREFIX
-wp core install --url=$WPURL/wp --title=YOUR_TITLE --admin_user=wordpress --admin_password=password --admin_email=email@example.com
+wp core install --url=$WPURL/wp --title=WP_CLI_CHANGE_SITEURL --admin_user=wordpress --admin_password=password --admin_email=$WPEMAIL
 wp option update home $WPURL
 cp index.php ../index.php && cd ..
 sed -i "" "s|/wp-blog-header|/wp/wp-blog-header|" ./index.php
